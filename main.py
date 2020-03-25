4325cee485894821d637c15696b82ecfcd1f4589
Unknown <walczak.jan@web.de>
2020-03-25 18:55:03 +0100

    add Sudoku and Solver class dummy
    Took 3 minutes

---
+++ main.py
@@ -0,0 +1,16 @@
+
+
+class Sudoku:
+
+    def __init__(self, s_string):
+        self.s_string = s_string
+
+    def __repr__(self):
+        pass
+
+
+class Solver:
+
+    def __init__(self):
+        pass
+
