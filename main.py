667314ab0fc0415452898f9a7522bbe5cab37d31
Unknown <walczak.jan@web.de>
2020-03-25 19:00:40 +0100

    add Sudoku docstring
    Took 5 minutes

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 
 
 class Sudoku:
-
+    """
+    The Sudoku class uses a row-wise string representation of a Sudoku of length 81 where the digit 0 represents empty
+    positions. The first digit of the string represents the position at the top-left and the last digit represents the
+    bottom right.
+    """
     def __init__(self, s_string):
         self.s_string = s_string
 
